1d3ea6737411c4bb47489130377f069d4739d305
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-11 09:24:51 -0300

    add - javascript

---
+++ script.js
@@ -0,0 +1,44 @@
+// console.log("Hello World")
+
+// let nome = "Pedro";
+// console.log(nome);
+
+// const pi ="3,14";
+// console.log(pi);
+
+// let n1 = 1;
+// let n2 = 2;
+
+// console.log(n1 + n2);
+
+// function somar(n3,n4){
+//     console.log(n3 + n4)
+// };
+
+// function saudacao(){
+//     console.log("Olá meu nome é " + nome);
+// };
+
+// somar(4,5);
+// saudacao();
+
+function somarNumeros(){
+    let soma = 0;
+    let continuar = true;
+
+    while(continuar){
+        let numero = parseFloat(prompt("Digite um número (ou zero para encerrar):"));
+
+
+        if(isNaN(numero)){
+            alert("Por favor, digite um número válido.")
+        } else {
+            soma += numero;
+        }
+
+        continuar = confirm("deseja adicionar mais um número?");
+    }
+    alert("A soma dos números é: " + soma);
+}
+
+somarNumeros();
