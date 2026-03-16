7c890b1ef935cac19894653516ff5fa041b1ebd4
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-16 07:58:38 -0300

    add  - exercio 3 e 4

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -18,3 +18,56 @@ function Exercicio_1(){
 }
 
 somarNumeros();
+
+function Exercicio_2(){
+   
+    for(let i = 10; i>=0;i--){
+        alert("Lançamento em: " + i);
+    }
+    alert("Lançamento realizado!");
+}
+
+function Exercicio_3() {
+
+let gasto = 0;
+let ganho = 0;
+let saldo = 0;
+
+
+for(let i = 1; i <= 12; i++){
+    gasto[i] += parseFloat(prompt("Informe o gasto bruto do mes: " + i))
+    ganho[i] += parseFloat(prompt("Informe o ganho bruto do mes: " + i))
+}
+saldo=bruto-gasto
+
+alert("O saldo financeiro aunal é de: R%" + saldo);
+alert("O ganho bruto anual foi de: R%" + bruto);
+alert("O gasto bruto anual foi de: R%" + gasto);
+
+}
+
+function Exercicio_4() {
+
+let vetor = [4];
+let aux;
+
+    for (let i = 0; i <4; i++){
+
+        vetor[i] = parseInt(prompt("Insira o valor: " + i));
+    }
+
+    for (i=0; i<4; i++){
+
+        for (let j=1; j<4;j++){
+
+           if(vetor[j] < vetor[j+1]){
+            aux = vetor[j];
+            vetor[j] = vetor[j+1];
+            vetor[j+1] = aux;
+           } 
+
+        }
+
+    }
+    alert("Ordenados em forma decrescente: " + vetor[1] + "; " + vetor[2] + "; " + vetor[3] + "; " + vetor[4]);
+}
--- Exercicio_2.js
+++
@@ -1,7 +0,0 @@
-function Exercicio_2(){
-   
-    for(let i = 10; i>=0;i--){
-        alert("Lançamento em: " + i);
-    }
-    alert("Lançamento realizado!");
-}
