b064781f480dae5802f9709725d2c4901a86d668
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-16 09:08:31 -0300

    add - Exercicio 11

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -195,3 +195,27 @@ alert("Valor do desconto INSS: " + inss);
 alert("Salário Líquido: R$" + (salario-inss));
 
 }
+
+function Exercicio_11(){
+
+let nome=0;
+let salario=0;
+let inss=0;
+
+nome = prompt("Nome do funcionário: ");
+salario = prompt("Salário Bruto: ");
+
+if(salario < 1000){
+    inss = salario*0.08;
+} else if(salario >= 1000 || salario <= 1500){
+    inss = salario*0.085;
+} else if(salario >= 1500){
+    inss = salario*0.09;
+}
+
+alert("Nome: " + nome);
+alert("Salário Bruto: R$" + salario);
+alert("Valor do desconto INSS: " + inss);
+alert("Salário Líquido: R$" + (salario-inss));
+
+}
