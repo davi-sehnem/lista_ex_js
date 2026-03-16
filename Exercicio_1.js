3ca05d135ec2e4f9bed7b26adfcf549c4f44e833
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-16 08:32:40 -0300

    add - Exercicio 7

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -98,3 +98,46 @@ if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra ===
 }
 
 }
+
+function Exercicio_7() {
+
+let sabor = prompt("Escolha um sabor de picole: \nA-Chocolate \nB-Morango \nC-Creme \nD-Manga \nE-Melancia \nF-Vanilla ice \nG-Céu Azul \nH-Brownie \nI-Hawaiano")
+
+if(sabor === 'A'){
+    alert("Sabor Choloate: R$1,50")
+    return;
+} 
+if(sabor === 'B'){
+    alert("Sabor Morango: R$2,50")
+    return;
+} 
+if(sabor === 'C'){
+    alert("Sabor Creme: R$2,50")
+    return;
+} 
+if(sabor === 'D'){
+    alert("Sabor Manga: R$3,20")
+    return;
+} 
+if(sabor === 'E'){
+    alert("Sabor Melancia: R$3,40")
+    return;
+} 
+if(sabor === 'F'){
+    alert("Sabor Vanilla Ice: R$3,00")
+    return;
+} 
+if(sabor === 'G'){
+    alert("Sabor Céu Azul: R$3,60")
+    return;
+} 
+if(sabor === 'H'){
+    alert("Sabor Brownie: R$4,00")
+    return;
+} 
+if(sabor === 'I'){
+    alert("Sabor Hawaiano: R$5,00")
+    return;
+} 
+
+}
