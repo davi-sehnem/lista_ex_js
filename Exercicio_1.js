45949441bda77d43877dc47c58746ad234efacaf
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-03-16 09:02:03 -0300

    add - Exercicio 10

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -177,3 +177,21 @@ if (n1 > n2){
 }
 
 }
+
+function Exercicio_10(){
+
+let nome=0;
+let salario=0;
+let inss=0;
+
+nome = prompt("Nome do funcionário: ");
+salario = prompt("Salário Bruto: ");
+
+inss += salario*0.08;
+
+alert("Nome: " + nome);
+alert("Salário Bruto: R$" + salario);
+alert("Valor do desconto INSS: " + inss);
+alert("Salário Líquido: R$" + (salario-inss));
+
+}
